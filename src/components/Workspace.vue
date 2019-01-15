67f5f824f288a64c128fede8e1072687a12ccd3c
Tim Kilian <mail@timkilian.pl>
2019-01-16 00:33:30 +0100

    Add renew-formalism-pt

--- src/components/Workspace.vue
+++ src/components/Workspace.vue
@@ -5,13 +5,15 @@
 </template>
 
 <script>
+import PluginPT from 'renew-formalism-pt';
 import Drawing from 'renew-lib';
 
 export default {
     props: {
     },
     mounted () {
-        new Drawing('workspace');
+        let drawing = new Drawing('workspace');
+        drawing.addFormalism(new PluginPT());
     },
 };
 </script>
